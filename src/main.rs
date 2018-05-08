c009d95bd3204efd416d4fc437bdb6420347ed27
Karsten Jeschkies <jeskar@web.de>
2018-05-08 15:09:13 +0200

    Start simple tcp server.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate tokio;
+
+use tokio::io;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:6142".parse().unwrap();
+    let listener = TcpListener::bind(&addr).unwrap();
+
+    let server = listener.incoming().for_each(|socket| {
+        println!("accepted socket; addr={:?}", socket.peer_addr().unwrap());
+
+        Ok(())
+    })
+    .map_err(|err| {
+        println!("accept error = {:?}", err);
+    });
+
+    println!("server running on localhost:6142");
+    tokio::run(server);
 }
